4ebd719c3152736c9c84f9004ed3cbe6b0cac95e
Valentin Magnan <19999588+valm13@users.noreply.github.com>
2021-09-01 17:45:49 +0200

    Generate MyNotesServer with Spring initializr

---
+++ MyNotesServer/src/main/java/com/dilitrust/recruitement/magnan/mynotes/server/MyNotesServerApplication.java
@@ -0,0 +1,13 @@
+package com.dilitrust.recruitement.magnan.mynotes.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyNotesServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyNotesServerApplication.class, args);
+    }
+
+}
---
+++ MyNotesServer/src/test/java/com/dilitrust/recruitement/magnan/mynotes/server/MyNotesServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dilitrust.recruitement.magnan.mynotes.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyNotesServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
